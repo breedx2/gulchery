17c573d363785c3550fbdb39f3c575d6da9582e7
jason <jason@noisybox.net>
2018-06-04 22:03:49 -0700

    blend in amenities

--- parse.js
+++ parse.js
@@ -1,16 +1,20 @@
 'use strict';
 const _ = require('lodash');
+const cheerio = require('cheerio');
 
 const parseString = require('xml2js').parseString;
 
 function parse(html){
+  const linkInfo = extractLinkInfo(html);
+  // console.log(JSON.stringify(linkInfo, null, '  '));
   return new Promise((fulfill, reject) => {
     const extracted = extractXml(html);
     parseString(extracted, (err, res) => {
       if(err){
         return reject(err);
       }
-      fulfill(munge(res));
+      const result = combine(linkInfo, munge(res));
+      fulfill(result);
     })
   });
 }
@@ -19,10 +23,30 @@ function extractXml(html){
   return html.replace(/^[\s\S]*<markers>(.*)<\/markers>[\s\S]*/, '<markers>$1</markers>');
 }
 
+function extractLinkInfo(html){
+  const $ = cheerio.load(html);
+  const infos = $('.searchParkBlk').map((i, div) => {
+    const link = $(div).find('.sheader').attr('href');
+    return {
+      name: $(div).find('.sheader').text(),
+      parksAndRecPage: `https://www.portlandoregon.gov${link}`,
+      amenities: $(div).find('.amenityImg').get().map(x => $(x).attr('title'))
+    };
+  }).get();
+  return _.keyBy(infos, 'name');
+}
+
 function munge(json){
   return json.markers.marker
     .map( x => x.$)
     .map(x => _.pickBy(x, _.identity));
+}
+
+function combine(linkInfo, markers){
+  return markers.map(x => {
+    const info = linkInfo[x.desc];
+    return _.assign(x, _.omit(info, 'name'));
+  });
 }
 
 module.exports = parse;
